665830e19b0d7882e77940fafc7fd56a4993c0d6
SohamGhugare <soham.ghugare@gmail.com>
2023-05-07 19:35:36 +0530

    Added Parse token logic

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -1,10 +1,40 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 func RequireAuth(c *gin.Context){
+	// Fetching the cookie
+	tokenString, err := c.Cookie("token")
+	if err!=nil{
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Login to access this route",
+		})
+	}
 
+	// Parsing and validating token
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
 	
+		return []byte(os.Getenv("SECRET")), nil
+	})
+	
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["sub"])
+	} else {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"message": "Invalid access token",
+		})
+	}
 
 	// Continuing
 	c.Next()
