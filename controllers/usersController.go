a29a6bee2baf9f58e883dd8599a5f84d1e6ce7ab
SohamGhugare <soham.ghugare@gmail.com>
2023-05-07 18:15:00 +0530

    Added Login method with JWT Auth

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -4,8 +4,11 @@ import (
 	"goauth/initializers"
 	"goauth/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,7 +21,7 @@ func Signup(c *gin.Context) {
 
 	if err := c.Bind(&body); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Failed to read request body",
+			"error": "Failed to create user, please try using another email.",
 		})
 
 		return
@@ -50,5 +53,69 @@ func Signup(c *gin.Context) {
 	// Responding
 	c.JSON(http.StatusOK, gin.H{
 		"user": user,
+	})
+}
+
+func Login(c *gin.Context) {
+
+	// Binding user email and password
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if err := c.Bind(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read request body",
+		})
+
+		return
+	}
+
+	// Finding the user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Email not found.",
+		})
+
+		return
+	}
+
+	// Verifying password by comparing hash
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Incorrect password.",
+		})
+
+		return
+	}
+
+	// Generating JWT Token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create token.",
+		})
+
+		return
+	}
+
+	// Responding by setting a cookie with token
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("token", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Authorized",
 	})
 }
